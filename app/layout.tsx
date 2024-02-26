67b1204426a6c03465a49a3e80b77a8515f810f7
Sophie Y <javachiphi@gmail.com>
2024-02-26 14:45:01 +0800

    set up user managmenet with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { IBM_Plex_Sans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import { ClerkProvider } from '@clerk/nextjs';
 
 import './globals.css';
 
@@ -22,10 +23,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: {
+          colorPrimary: '#624cf5',
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
