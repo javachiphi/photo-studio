d61e203ed26974bf355a6137204d249d25de8076
Sophie Y <javachiphi@gmail.com>
2024-02-28 11:19:08 +0800

    prepare transfrom config based on type

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -194,6 +194,12 @@ const TransformationForm = ({
     });
   };
 
+  useEffect(() => {
+    if (image && (type === 'restore' || type === 'removeBackground')) {
+      setNewTransformation(transformationType.config);
+    }
+  }, [image, transformationType.config, type]);
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
